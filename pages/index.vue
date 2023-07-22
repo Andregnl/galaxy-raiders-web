9640f1dc834b0c436b2fac231ab9bc46f51b2df3
André Gustavo Nakagomi Lopez <andregnl@usp.br>
2023-07-22 20:39:12 -0300

    Add main menu
    - Add Leaderboard page
    - Add New Game button
    - Add Exit button
    
    (Altered p2 branch of backend so it sends default Leaderboard)
    (NewGame button and Exit button both send a command to backend, but
    backend wasn't programmed to receive these signals)
    
    Signed-off-by: André Gustavo Nakagomi Lopez <andregnl@usp.br>

--- pages/index.vue
+++ pages/index.vue
@@ -1,115 +1,38 @@
 <template>
-  <div id="canvas">
-    <div id="deep-space" />
-    <div id="space-field">
-      <SpaceObject id="spaceship" class="spaceship" :data="spaceField.ship" resolution="2" />
-
-      <SpaceObject class="asteroid" :data="asteroid" resolution="2"
-                  :key="asteroid.center"
-                  v-for="asteroid in spaceField.asteroids" />
-
-      <SpaceObject class="missile" :data="missile" resolution="2"
-                  :key="missile.center"
-                  v-for="missile in spaceField.missiles" />
-
-      <SpaceObject class="explosion" :data="explosion" resolution="2"
-                  :key="explosion.center"
-                  v-for="explosion in spaceField.explosions" />
-    </div>
-  </div>
+    <h1>
+    <nav>
+      <ul>
+        <div><NuxtLink to="/game"><button class="menu" @click="newGame">New Game</button></NuxtLink></div>
+        <div><NuxtLink to="/leaderboard"><button class="menu">Leaderboard</button></NuxtLink></div>
+        <div><button class="menu" @click="exitGame">Exit</button></div>
+      </ul>
+    </nav>
+    </h1>
 </template>
 
 <script setup>
-const {
-  data: spaceField,
-  refresh: updateSpaceField
-} = await $get("/space-field");
 
-onMounted(() => {
-  window.addEventListener("keydown", async (event) => {
-    const keyToCommand = {
-      "ArrowUp": "MOVE_SHIP_UP",
-      "ArrowDown": "MOVE_SHIP_DOWN",
-      "ArrowRight": "MOVE_SHIP_RIGHT",
-      "ArrowLeft": "MOVE_SHIP_LEFT",
-      "Space": "LAUNCH_MISSILE",
-      "Escape": "PAUSE_GAME",
-    };
+function newGame() {
+  console.log("Start new game");
+  $post("/menu", "new game")
+}
 
-    const command = keyToCommand[event.code];
+function exitGame() {
+  console.log("Exit game");
+  $post("/menu", "exit")
+}
 
-    // Ignore if invalid key was pressed
-    if (command === undefined) return;
-
-    console.log(`Triggering command: ${command}`);
-    await $post("/ship/commands", { command })
-  });
-
-  window.setInterval(updateSpaceField, 1000);
-})
 </script>
 
+
 <style>
-#canvas {
-  height: calc(100vh - 4rem);
-  width: calc(100vw - 4rem);
-
-  padding: 2rem;
-
-  background-color: #36bbf5;
-  overflow: hidden;
-
-  position: relative;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
+h1 {
+  text-align: center;
+  margin-top: 4em;
 }
 
-@keyframes slide {
-  0% {
-    transform: translate(1px);
-  }
-  50% {
-    transform: translate(-1px);
-  }
-  100% {
-    transform: translate(1px);
-  }
-}
-
-#deep-space {
-  height: calc(100% - 4rem);
-  width: calc(100% - 4rem);
-
-  background-image: url("~/assets/space.png");
-  background-origin: content-box;
-  animation: slide 3s linear infinite;
-
-  position: absolute;
-  z-index: 0;
-}
-
-#space-field {
-  height: calc(100% - 4rem);
-  width: calc(100% - 4rem);
-
-  position: relative;
-}
-
-.spaceship {
-  background-image: url("~/assets/spaceship.png");
-}
-
-.asteroid {
-  background-image: url("~/assets/asteroid.png");
-}
-
-.missile {
-  background-image: url("~/assets/missile.png");
-}
-
-.explosion {
-  background-image: url("~/assets/explosion.png");
+.menu {
+  height: 50px;
+  width: 100px;
 }
 </style>
---
+++ pages/leaderboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <font size="+1">
+  <div class="Leaderboard">
+  <div v-for="(value, name) in matches.matches">
+    entry: {{ name }}
+    <div v-for="(entry, name) in value">
+      {{ name  }}: {{ entry  }}
+    </div>
+  </div>
+	<nav>
+      <ul>
+        <NuxtLink to="/"><button class="menu">Back</button></NuxtLink>
+      </ul>
+	</nav>
+  </div>
+  </font>
+</template>
+
+
+<script setup>
+const {
+  data: matches
+} = await $get("/leaderboard");
+
+console.log(matches)
+</script>
+
+<style>
+
+.Leaderboard {
+  text-align: center;
+}
+
+</style>
